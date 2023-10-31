7f4d00a01d99e6363ad2a56cfc9a58c6b30b5f26
natimansilla <natimmansilla@gmail.com>
2023-10-30 21:37:21 -0300

    Add funcion menu

---
+++ main.py
@@ -0,0 +1,14 @@
+def menu():
+    opciones = ("====MENU DE OPCIONES====\n"
+                "1.\n"
+                "2.\n"
+                "3.\n"
+                "4.\n"
+                "5.\n"
+                "6.\n"
+                "7.\n"
+                "8.\n"
+                "0.Salir\n"
+                "====> Ingrese su opción: ")
+    return int(input(opcines))
+

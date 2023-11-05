4626daa5a186c3ca524c1f2a6040604ac17c8ce8
natimansilla <natimmansilla@gmail.com>
2023-11-05 15:51:42 -0300

    Version final

--- Evento.py
+++ Evento.py
@@ -14,41 +14,46 @@ class Evento:
         return "ID: {:<10} | Titulo: {:<15} | Descripción: {:<88} | Costo: $ {:<13} | Tipo de Evento: {:<4} |" \
                " Segmento diario: {:<3} "\
             .format(self.codigo, self.titulo, self.descripcion, self.costo, self.tipo_evento, self.segmento)
+
+
 def generar_codigo():
     letras = ("AED", "JCB", "ETC", "HID", "OPE", "YQF", "AGX", "BGH", "OPÑ")
     codigo = random.choice(letras) + random.choice(letras) + str(random.randint(100, 999))
     return codigo
 
+
 def generar_titulo():
-    secciones = ("Informativo", "Ultimo minuto", "Recordatorio", "Aviso Urgente")
+    secciones = ("Informativo", "Último minuto", "Recordatorio", "Aviso Urgente")
     titulos = random.choice(secciones)
     return titulos
 
+
 def generar_descripcion(titulo):
-    token = random.randint(1,2)
+    descripcion = ""
+    token = random.randint(1, 2)
     if titulo == "Informativo":
         if token == 1:
             descripcion = "Precio de BTC cae por sobrecalentamiento de derivados de bitcoin."
         elif token == 2:
             descripcion = "Encontró un barco de 1886 sepultado en Tigre y hoy viaja para devolverlo a su tierra."
 
-    elif titulo == "Ultimo minuto":
+    elif titulo == "Último minuto":
         if token == 1:
             descripcion = "Fifa confirmó las 16 sedes que tendrá el Mundial 2026."
         if token == 2:
-            descripcion = "El terremoto tuvo su epicentro en San Juan y se sintio en todo el norte argentino."
+            descripcion = "El terremoto tuvo su epicentro en San Juan y se sintió en todo el norte argentino."
 
     elif titulo == "Recordatorio":
         if token == 1:
-            descripcion = "Ya estan abiertas las convocatorias para el financiamiento de proyectos tecnologicos."
+            descripcion = "Ya estan abiertas las convocatorias para el financiamiento de proyectos tecnológicos."
         elif token == 2:
             descripcion = "Hasta el viernes 1 de diciembre podes inscribirte para el ingreso 2024 en la UTN."
 
     elif titulo == "Aviso Urgente":
         if token == 1:
-            descripcion = "Corte de ruta genera demoras en la Autopista Buenos Aires Rosario."
+            descripcion = "Cortes de ruta generan demoras en la Autopista Buenos Aires Rosario."
         elif token == 2:
-            descripcion = "Por faltante de combustible, en Tartagal suspendieron el servicio de transporte."
+            descripcion = "En Tartagal por faltante de combustible suspendieron el servicio de transporte."
 
     return descripcion
 
@@ -64,4 +69,3 @@ def generar_aleatorio():
     evento = Evento(codigo, titulo, descripcion, costo, tipo_evento, segmento)
 
     return evento
-
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import pickle
-
-from Evento import Evento
 from Evento import generar_aleatorio
 import os
+import pickle
+
 
 def menu():
     print()
@@ -25,17 +24,19 @@ def validar_mayor_que(limite, mensaje="Ingrese un numero: "):
     while numero <= limite:
         numero = int(input(mensaje))
         if numero <= limite:
-            print("ERROR! Debe ingresar un numero mayor que {}".format(limite))
+            print("ERROR! Debe ingresar un número mayor a {}".format(limite))
     return numero
 
 
-#PUNTO 1
+# PUNTO 1
 def generar_vector(vec, n):
     for i in range(n):
         evento = generar_aleatorio()
         add_in_order(evento, vec)
     print("Los eventos fueron cargados exitosamente...")
 
+
+# Inserta ordenadamente por código.
 def add_in_order(evento, vec):
     n = len(vec)
     izq, der = 0, n - 1
@@ -54,20 +55,36 @@ def add_in_order(evento, vec):
 
     vec[pos:pos] = [evento]
 
+
 # PUNTO 2
 def mostrar_vector(vec):
     for eventos in vec:
         print(eventos)
 
+
 # PUNTO 3
-def generar_archivo_binario(vec, archivo_binario):
+def generar_archivo_binario(vec, archivo_binario, p):
+    # p es un monto/costo de producción cargado por teclado
     mb = open(archivo_binario, "wb")
     for eventos in vec:
-        pickle.dump(eventos, mb)
+        if eventos.costo > p:
+            pickle.dump(eventos, mb)
 
     mb.close()
     print("Archivo binario exitosamente generado...")
 
+
+def varlidar_rango_float(inferior, superior, mensaje="Ingrese un número: "):
+    numero = inferior - 1
+    while numero < inferior or numero > superior:
+        numero = float(input(mensaje))
+        if numero < inferior or numero > superior:
+            print("ERROR! Debe ingresar un número que sea mayor o igual que {} y menor o igual que {}."
+                  .format(inferior, superior))
+    return numero
+
+
+# PUNTO 4
 def mostrar_archivo_binario(archivo_binario):
     if os.path.exists(archivo_binario):
         mb = open(archivo_binario, "rb")
@@ -81,8 +98,8 @@ def mostrar_archivo_binario(archivo_binario):
         print("El archivo binario {} no existe...".format(archivo_binario))
 
 
-# PUNTO 4
-def generar_vector_acumulacion(vec_acum, archivo_binario):
+# PUNTO 5
+def generar_vector_costo(vec_costo, archivo_binario):
     if os.path.exists(archivo_binario):
         mb = open(archivo_binario, "rb")
         tam = os.path.getsize(archivo_binario)
@@ -91,29 +108,29 @@ def generar_vector_acumulacion(vec_acum, archivo_binario):
             evento = pickle.load(mb)
 
             if evento.tipo_evento >= 5:
-                vec_acum.append(evento.costo)
+                vec_costo.append(evento.costo)
 
         mb.close()
     else:
         print("El archivo binario {} no existe...".format(archivo_binario))
 
 
-def mostrar_vector_acumulacion(vec_acum):
+def mostrar_vector_costo(vec_costo):
     contador_eventos = 0
     acumulador_costo = 0
     promedio = 0
-    for eventos in vec_acum:
+    for eventos in vec_costo:
         contador_eventos += 1
         acumulador_costo += eventos
         print("Costo: $ ", eventos)
 
     if contador_eventos != 0:
         promedio = round(acumulador_costo / contador_eventos, 2)
-
+    print()
     print("El promedio de los montos mostrados es de $ {}".format(promedio))
 
 
-# PUNTO 5
+# PUNTO 6
 def validar_codigo(mensaje="Ingrese un mensaje: "):
     codigo = ""
     while len(codigo) != 9:
@@ -141,32 +158,35 @@ def busqueda_binaria_x_codigo(cod, vec):
 
 
 # PUNTO 7
-def cargar_matriz_conteo(mat_conteo, vec):
+def cargar_matriz_conteo(vec):
     """
     fila = tipo_evento = [0;19] => 20
     columna = segmento = [0;9] => 10
     total 10*20 = 200 contadores
     """
-    mat = [[0]*10 for i in range (20)]
+    mat_conteo = [[0]*10 for i in range(20)]
     for evento in vec:
         fila = evento.tipo_evento
         columna = evento.segmento
-        mat[fila][columna] += 1
+        mat_conteo[fila][columna] += 1
+
+    return mat_conteo
 
 
-def varlidar_rango(inferior, superior, mensaje="Ingrese un numero: "):
-    numero = inferior
+def varlidar_rango_int(inferior, superior, mensaje="Ingrese un número: "):
+    numero = inferior - 1
     while numero < inferior or numero > superior:
         numero = int(input(mensaje))
         if numero < inferior or numero > superior:
-            print("ERROR! Debe ingresar un numero que sea mayor o igual que {} y menor o igual que {}."
+            print("ERROR! Debe ingresar un número que sea mayor o igual que {} y menor o igual que {}."
                   .format(inferior, superior))
     return numero
+
 
 def mostrar_matriz_conteo(mat_conteo, te):
     for i in range(len(mat_conteo)):
         for j in range(len(mat_conteo[i])):
-            if evento.tipo_evento > te:
+            if i > te and mat_conteo[i][j] > 0:
                 print("Para el tipo de evento {} y el segmento diario {}, los eventos son: {}"
                       .format(i, j, mat_conteo[i][j]))
 
@@ -195,7 +215,8 @@ def calcular_cantidad_palabras(cadena):
             if car.lower() == "s":
                 c_tiene_s += 1
 
-        else: # analizo palabras
+        # Analizo palabras
+        else:
             if ccp > 0:
                 cp += 1
                 if b_start_mayus and (c_tiene_t > 0) and (c_tiene_s > 0):
@@ -214,10 +235,12 @@ def es_mayuscula(c):
 
 
 def principal():
+    print()
     vec = []
-    mat_conteo = []
     op = -1
     archivo_binario = "eventos.dat"
+    b_op_6 = False
+    pos = None
 
     while op != 0:
         op = menu()
@@ -232,39 +255,51 @@ def principal():
                 mostrar_vector(vec)
 
             elif op == 3:
-                generar_archivo_binario(vec, archivo_binario)
+                # p es un monto/costo de producción cargado por teclado
+                p = varlidar_rango_float(100000, 9999999, "Ingrese el monto mínimo de "
+                                                          "producción: ")
+                generar_archivo_binario(vec, archivo_binario, p)
 
             elif op == 4:
                 mostrar_archivo_binario(archivo_binario)
 
             elif op == 5:
-                vec_acum = []
-                generar_vector_acumulacion(vec_acum, archivo_binario)
-                mostrar_vector_acumulacion(vec_acum)
+                vec_costo = []
+                generar_vector_costo(vec_costo, archivo_binario)
+                mostrar_vector_costo(vec_costo)
 
             elif op == 6:
+                b_op_6 = True
                 cod = validar_codigo("Ingrese el código a buscar: ")
                 pos = busqueda_binaria_x_codigo(cod, vec)
                 if pos != -1:
                     print(vec[pos])
-                    print("Descripcion del evento: ", vec[pos].descripcion)
+                    print()
+                    print("Descripción del evento: ", vec[pos].descripcion)
                 else:
-                    print("El codigo {} no se encontró.".format(cod))
+                    print("El código {} no se encontró.".format(cod))
+                    print("No existe..")
 
             elif op == 7:
-                cargar_matriz_conteo(mat_conteo, vec)
+                mat_conteo = cargar_matriz_conteo(vec)
+
                 # te es un tipo de evento [0;19] que se carga por teclado
-                te = varlidar_rango(0, 19, "Ingrese el tipo de evento a discriminar: ")
+                te = varlidar_rango_int(0, 19, "Ingrese el tipo de evento mínimo: ")
                 mostrar_matriz_conteo(mat_conteo, te)
 
-            elif op == 8:
-                cadena = vec[pos].descripcion
-                contador_palabras = calcular_cantidad_palabras(cadena)
+            elif b_op_6:
+                if op == 8:
+                    cadena = vec[pos].descripcion
+                    contador_palabras = calcular_cantidad_palabras(cadena)
 
-                print("La cantidad de palabras que empiezan con mayúscula, contienen una letra t y una letra s son {}"
-                      .format(contador_palabras))
+                    print("La cantidad de palabras que empiezan con mayúscula, contienen una letra t y una letra s "
+                          "son {}".format(contador_palabras))
+            else:
+                print("Debe cargar la descripción antes. Ingrese la opción 6.")
+
         elif op == 0:
             print("Fin del programa! ")
+
         else:
             print("Debe cargar los eventos primero! Ingrese la opción 1.")
 
